fa9a6bd018aa20c1bcf213012e618c68d2f4ef61
Jiri Suchomel <suchomelj97@gmail.com>
2023-12-22 07:29:36 +0100

    create new commands, test data and selectors, implement into primitive homepage tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -19,3 +19,32 @@ Cypress.Commands.add("apiLogin", (username: String, password: string) => {
     // window.localStorage.setItem("authToken", body.token);
   });
 });
+
+// assertion of single attribute of given element
+Cypress.Commands.add(
+  "assertAttrib",
+  (el: string, attrib: string, value: string) => {
+    if (el && attrib && value) {
+      cy.get(el).should("have.attr", attrib, value);
+    }
+  }
+);
+
+// assertion of visibility of given element
+Cypress.Commands.add("assertVisibility", (el: string, bool: boolean) => {
+  switch (bool) {
+    case true:
+      cy.get(el).should("be.visible");
+      break;
+    case false:
+      cy.get(el).should("not.be.visible");
+      break;
+    default:
+      throw new Error("ERROR in boolean handling logic.");
+  }
+});
+
+// assertion of content of given element
+Cypress.Commands.add("assertContent", (el: string, content: string) => {
+  cy.get(el).should("contain", content);
+});
---
+++ cypress/testdata/testData.ts
@@ -0,0 +1,47 @@
+type stringNavbar = {
+  signUp: string;
+  logIn: string;
+  cart: string;
+  aboutUs: string;
+  contact: string;
+  home: string;
+  productStore: string;
+};
+
+type stringCategories = {
+  header: string;
+  phones: string;
+  notebooks: string;
+  monitors: string;
+};
+
+// strings for Navbar
+export const stringNavbar = {
+  signUp: "Sign up",
+  logIn: "Log in",
+  cart: "Cart",
+  aboutUs: "About us",
+  contact: "Contact",
+  home: "Home",
+  productStore: "PRODUCT STORE",
+};
+
+// strings for Categories
+export const stringCategories = {
+  header: "CATEGORIES",
+  phones: "Phones",
+  notebooks: "Laptops",
+  monitors: "Monitors",
+};
+
+// strings for Footer
+export const stringFooter = {
+  aboutUs: "About Us",
+  aboutUsContent: `We believe performance needs to be validated at every stage of the software development cycle and our open source compatible, massively scalable platform makes that a reality.`,
+  contact: "Get in Touch",
+  contactContentAdd: "Address: 2390 El Camino Real",
+  contactContentPhone: "Phone: +440 123456",
+  contactContentEmail: "Email: demo@blazemeter.com ",
+  productStore: " PRODUCT STORE", // left the space on purpose there, it looks like a design choice to not have string too close to the logo
+  copyright: "Copyright © Product Store 2017",
+};
